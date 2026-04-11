66a21d1029e1d75edd35ece4bb372ef2b360e555
jadon au-yeung <aycjadon@gmail.com>
2026-04-11 14:05:08 -0400

    slightest change

--- backend/main.py
+++ backend/main.py
@@ -66,7 +66,7 @@ allowed_origins = [
 ]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins="https://canadian-athlete-directory-r6x6.vercel.app",
+    allow_origins=["https://canadian-athlete-directory-r6x6.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
